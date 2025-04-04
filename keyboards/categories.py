23393516844f856b88d4e26b1bfb2a084bd3c086
ilya3052 <ipopadin5@gmail.com>
2025-04-04 17:38:01 +0300

    ref: текст на кнопках + добавление новой кнопки и эмодзи

--- keyboards/categories.py
+++ keyboards/categories.py
@@ -4,7 +4,14 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_categories_kb(data: list[str]):
     builder = InlineKeyboardBuilder()
     for item in data:
-        builder.button(text=item, callback_data=f"category_{item}")
+        builder.button(text=item[0], callback_data=f"category_{item[0]}")
     builder.adjust(3)
+
+    static_builder = InlineKeyboardBuilder()
+    static_builder.button(text="◀️ Предыдущая", callback_data="action_previous"),
+    static_builder.button(text="Следующая ▶️", callback_data="action_next")
+    static_builder.button(text="Закрыть каталог ❌", callback_data="action_close")
+    static_builder.adjust(2)
+    builder.attach(static_builder)
 
     return builder.as_markup()
