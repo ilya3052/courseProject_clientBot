150b1abc5d82440acdc6366e9471d9a49e1a9efc
ilya3052 <ipopadin5@gmail.com>
2025-04-04 17:36:31 +0300

    ref : список товаров получается автоматически, а не задан железно в коде\n в обработчики callback-запросов добавлены фильтры на состояние\n логика хранения текущей страницы в каталоге/списки товаров разделена, за каждое состояние отвечает своя переменная\n добавлены обработчики действий по возврату в каталог и закрытию каталога\n логика отображения категорий вынесена в отдельную функцию\n

--- handlers/catalog.py
+++ handlers/catalog.py
@@ -10,7 +10,8 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message, CallbackQuery
 from psycopg import sql
 
-from keyboards.products import get_products
+from keyboards.categories import get_categories_kb
+from keyboards.product_list import get_products_list_kb
 from shared.database import Database
 
 router = Router()
@@ -24,21 +25,23 @@ class Catalog(StatesGroup):
     select_product = State()
 
 
-categories_list = ['Сумки и аксессуары', 'Кухонные принадлежности', 'Гигиена и уход', 'Электроника', 'Освещение',
-                   'Книги и журналы', 'Товары для спорта', 'Товары для дома']
-
-
 @router.message(StateFilter(None), Command("catalog"))
 async def cmd_catalog(message: Message, state: FSMContext):
-    from keyboards.categories import get_categories_kb
-    await message.answer(
-        "Выберите категорию товаров",
-        reply_markup=get_categories_kb(categories_list)
-    )
-    await state.set_state(Catalog.select_category)
+    connect: ps.connect = Database.get_connection()
+    select_categories = """SELECT DISTINCT product_category FROM product"""
+    with connect.cursor() as cur:
+        try:
+            categories_list = cur.execute(select_categories).fetchall()
+            await state.update_data(
+                categories_list=categories_list,
+                category_page=0,
+            )
+            await show_categories(state, message=message)
+        except ps.Error as e:
+            logging.error(e)
 
 
-@router.callback_query(F.data.startswith("category_"))
+@router.callback_query(F.data.startswith("category_"), StateFilter(Catalog.select_category))
 async def get_category(callback: CallbackQuery, state: FSMContext):
     connect: ps.connect = Database.get_connection()
     category = callback.data.split("_")[1]
@@ -50,44 +53,97 @@ async def get_category(callback: CallbackQuery, state: FSMContext):
             products = cur.execute(select_products.format(callback.data.split("_")[1])).fetchall()
             await state.update_data(
                 products_list=products,
-                page=0,
+                product_page=0,
                 category=category
             )
             await show_products(callback, state)
         except ps.Error as e:
-            pass
+            logging.error(e)
+
+
+@router.callback_query(F.data.startswith("action_"), StateFilter(Catalog.select_category))
+async def category_action(callback: CallbackQuery, state: FSMContext):
+    data = await state.get_data()
+    page = data.get('category_page', 0)
+    match callback.data.split("_")[1]:
+        case "next":
+            if (page + 1) * page_size < len(data['categories_list']):
+                await state.update_data(category_page=page + 1)
+        case "previous":
+            if page > 0:
+                await state.update_data(category_page=page - 1)
+        case "close":
+            await state.clear()
+            await callback.message.delete()
+            await callback.answer()
+            return
+
+    await show_categories(state, callback=callback)
+    await callback.answer()
 
 
 @router.callback_query(F.data.startswith("action_"), StateFilter(Catalog.show_products))
-async def action(callback: CallbackQuery, state: FSMContext):
+async def product_action(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
-    page = data['page']
+    page = data.get('product_page', 0)
     match callback.data.split("_")[1]:
         case "next":
             if (page + 1) * page_size < len(data['products_list']):
-                await state.update_data(page=page + 1)
+                await state.update_data(product_page=page + 1)
+            await show_products(callback, state)
         case "previous":
             if page > 0:
-                await state.update_data(page=page - 1)
+                await state.update_data(product_page=page - 1)
+            await show_products(callback, state)
+        case "back":
+            await state.set_state(Catalog.select_category)
+            await show_categories(state, callback)
+            return
 
-    await show_products(callback, state)
     await callback.answer()
+
+
+async def show_categories(state: FSMContext, callback: CallbackQuery = None, message: Message = None):
+    data = await state.get_data()
+    page = data.get('category_page', 0)
+    categories = data['categories_list']
+    try:
+        if callback:
+            await callback.message.edit_text(
+                "Выберите категорию товаров",
+                reply_markup=get_categories_kb(categories[page_size * page:page_size * (page + 1)])
+            )
+        else:
+            await message.answer(
+                "Выберите категорию товаров",
+                reply_markup=get_categories_kb(categories[page_size * page:page_size * (page + 1)])
+            )
+        await state.set_state(Catalog.select_category)
+    except TelegramBadRequest as TBR:
+        logging.info(TBR)
 
 
 async def show_products(callback: CallbackQuery, state: FSMContext):
     data = await state.get_data()
     products = data['products_list']
-    page = data['page']
+    page = data.get('product_page', 0)
     category = data['category']
     try:
         await callback.message.edit_text(
             f"Товары категории {category}\n",
-            reply_markup=get_products(products[page_size * page:page_size * (page + 1)])
+            reply_markup=get_products_list_kb(products[page_size * page:page_size * (page + 1)])
         )
         await state.set_state(Catalog.show_products)
     except TelegramBadRequest as TBR:
         logging.info(TBR)
 
-# @router.callback_query(F.data.startswith("product_"))
-# async def show_products(message: Message, state: FSMContext):
-#     print("Показываем продукты")
+
+@router.callback_query(F.data.startswith("product_"), StateFilter(Catalog.show_products))
+async def show_product(callback: CallbackQuery, state: FSMContext):
+    print("Показываем продукты")
+    await callback.answer()
+
+
+@router.callback_query(StateFilter(None))
+async def handle_no_action(callback: CallbackQuery):
+    await callback.answer()
