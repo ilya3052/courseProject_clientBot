217a345ea281de7ae9423c25dac2fbed7ec2d5d2
ilya3052 <ipopadin5@gmail.com>
2025-04-11 23:34:10 +0300

    add: новые кнопки; ref: положение кнопок

--- keyboards/product_info.py
+++ keyboards/product_info.py
@@ -4,19 +4,21 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 def get_product_info_kb(added_to_cart: bool = False):
     builder = InlineKeyboardBuilder()
     if added_to_cart:
-        builder.button(text="-", callback_data="count_decr")
+        builder.button(text="-", callback_data="count_dec")
         builder.button(text="+", callback_data="count_inc")
+        builder.button(text="◀️ Предыдущая", callback_data="action_previous"),
+        builder.button(text="Следующая ▶️", callback_data="action_next")
+        builder.button(text="Подтвердить заказ ✅", callback_data="action_confirm")
+        builder.button(text="Вернуться назад ↩️️", callback_data="action_back")
+        builder.button(text="Отменить заказ ❌", callback_data="action_cancel")
+        builder.adjust(2, 2, 1, 1, 1)
     else:
         builder.button(text="Добавить в корзину 🛒", callback_data="action_addToCart")
-    builder.adjust(2)
+        builder.button(text="◀️ Предыдущая", callback_data="action_previous"),
+        builder.button(text="Следующая ▶️", callback_data="action_next")
+        builder.button(text="Вернуться назад ↩️️", callback_data="action_back")
+        builder.button(text="Отменить заказ ❌", callback_data="action_cancel")
+        builder.adjust(1, 2, 1, 1)
 
-    static_builder = InlineKeyboardBuilder()
-    static_builder.button(text="◀️ Предыдущая", callback_data="action_previous"),
-    static_builder.button(text="Следующая ▶️", callback_data="action_next")
-    static_builder.button(text="Вернуться назад ↩️️", callback_data="action_back")
-    static_builder.button(text="Отменить заказ ❌", callback_data="action_cancel")
-    static_builder.adjust(2, 1, 1)
-
-    builder.attach(static_builder)
 
     return builder.as_markup()
