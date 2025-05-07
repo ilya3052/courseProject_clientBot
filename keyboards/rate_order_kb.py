640d1fbc72da3b41776a0ee721369dafa7a8456b
ilya3052 <ipopadin5@gmail.com>
2025-05-07 16:55:10 +0300

    add: клавиатура для оценки заказа

--- keyboards/rate_order_kb.py
+++ keyboards/rate_order_kb.py
@@ -1,10 +1,11 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-def rate_courier_kb():
+def get_rate_order_kb():
     builder = InlineKeyboardBuilder()
     for grade in range(1, 6):
         builder.button(text='⭐' * grade, callback_data=f'grade_{grade}')
-    builder.adjust(3, 2)
+    builder.button(text="Вернуться назад ↩️️", callback_data="action_back")
+    builder.adjust(3, 2, 1)
 
     return builder.as_markup()
