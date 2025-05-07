0045a9cdf4b0b0d772cd53b6e31e2caab4c1fd6c
ilya3052 <ipopadin5@gmail.com>
2025-05-07 16:55:00 +0300

    add: клавиатура для информации о заказе

---
+++ keyboards/order_info_kb.py
@@ -0,0 +1,12 @@
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+
+
+def order_info_kb(status):
+    builder = InlineKeyboardBuilder()
+    if status == 1:
+        builder.button(text="Подтвердить получение ✅", callback_data="action_confirmReceipt")
+    builder.button(text="Вернуться назад ↩️️", callback_data="action_back")
+    builder.adjust(1)
+
+
+    return builder.as_markup()
