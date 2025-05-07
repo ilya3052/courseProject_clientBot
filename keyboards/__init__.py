df1a75050d4ff5fbad8d72666e7749ff51270ca7
ilya3052 <ipopadin5@gmail.com>
2025-05-07 16:54:26 +0300

    add: импорт клавиатур информации о заказе и рейтинге

---
+++ keyboards/__init__.py
@@ -0,0 +1,7 @@
+from .categories_list_kb import get_categories_kb
+from .product_info_kb import get_product_info_kb
+from .product_list_kb import get_products_list_kb
+from .profile_kb import get_profile_kb
+from .orders_list_kb import get_orders_list_kb
+from .order_info_kb import order_info_kb
+from .rate_order_kb import get_rate_order_kb
