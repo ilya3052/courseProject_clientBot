8c942a47856ae02fc0b55516e0967cd7cf31e481
ilya3052 <ipopadin5@gmail.com>
2025-05-16 23:48:22 +0300

    add: функция получения уведомлений с канала

---
+++ handlers/deliveries.py
@@ -0,0 +1,7 @@
+from handlers import send_notify
+
+
+async def get_notify(conn, pid, channel, payload):
+    order_id = int(str(payload).split(":")[1].strip())
+    print(f"[{channel}] => {payload} => {order_id}")
+    await send_notify(order_id)
