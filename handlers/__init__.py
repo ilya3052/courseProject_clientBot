52ff3bb1bf3af3080bcaf1228d459c685d4b22f4
ilya3052 <ipopadin5@gmail.com>
2025-05-16 23:46:37 +0300

    add: дополнительные импорты

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,3 +1,4 @@
 from .catalog import router as catalog_router
-from .profile import router as profile_router
+from .profile import router as profile_router, send_notify
 from .register import router as reg_router
+from .deliveries import get_notify
--- keyboards/__init__.py
+++ keyboards/__init__.py
@@ -5,3 +5,4 @@ from .product_info_kb import get_product_info_kb
 from .product_list_kb import get_products_list_kb
 from .profile_kb import get_profile_kb
 from .rate_order_kb import get_rate_order_kb
+from .deliveries_kb import get_delivery_kb
