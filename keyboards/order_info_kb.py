ea0462e9f157c9b6d52bda5afd5147e26b855e86
ilya3052 <ipopadin5@gmail.com>
2025-05-17 13:48:53 +0300

    add: кнопка для удаления заказа

--- keyboards/order_info_kb.py
+++ keyboards/order_info_kb.py
@@ -3,7 +3,9 @@ from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 def order_info_kb(status):
     builder = InlineKeyboardBuilder()
-    if status == 1:
+    if status == 0:
+        builder.button(text="Отказаться от заказа ❌", callback_data="action_cancelOrder")
+    elif status == 1:
         builder.button(text="Подтвердить получение ✅", callback_data="action_confirmReceipt")
     builder.button(text="Вернуться назад ↩️️", callback_data="action_back")
     builder.adjust(1)
