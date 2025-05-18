351e65893c512897a182638f8bb36902879293ae
ilya3052 <ipopadin5@gmail.com>
2025-05-18 16:09:17 +0300

    Revert "ref: оптимизация проекта - структурные файлы перенесены в папку core"
    This reverts commit 9159463a2dc178dc0a140dd14d97f8b217fe267f.

--- core/__init__.py
+++
@@ -1,5 +0,0 @@
-from .bot import setup_bot
-from .bot_instance import bot
-from .database import Database
-from .logger import setup_logger
-from .notify import setup_notifications
--- handlers/register.py
+++ handlers/register.py
@@ -9,7 +9,7 @@ from aiogram.fsm.state import StatesGroup, State
 from aiogram.types import Message
 from psycopg import sql
 
-from core import Database
+from database import Database
 
 router = Router()
 
