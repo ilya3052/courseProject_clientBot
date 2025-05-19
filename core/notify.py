5554f541156100541a08d81c1bcd53a3090a1817
ilya3052 <ipopadin5@gmail.com>
2025-05-19 16:19:35 +0300

    ref: имя канала для уведомлений

--- core/notify.py
+++ core/notify.py
@@ -4,4 +4,4 @@ from handlers import get_notify
 
 async def setup_notifications():
     await Database.listen_channel("order_accept", get_notify)
-    await Database.listen_channel("order_not_accept", get_notify)
+    await Database.listen_channel("order_status", get_notify)
