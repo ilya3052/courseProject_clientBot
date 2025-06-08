a9de3ff040040c5987f0f4975e027904ca455be1
ilya3052 <ipopadin5@gmail.com>
2025-06-08 20:24:50 +0300

    ref: venv -> .venv

--- first_launch.bat
+++ first_launch.bat
@@ -1,11 +1,11 @@
 @echo on
 
-python -m venv venv
-call venv\Scripts\activate.bat
+python -m venv .venv
+call .venv\Scripts\activate.bat
 pip install -r requirements.txt
 
 echo @echo on > run.bat
-echo call venv\Scripts\activate.bat >> run.bat
+echo call .venv\Scripts\activate.bat >> run.bat
 echo python main.py >> run.bat
 
 del "%~f0"
