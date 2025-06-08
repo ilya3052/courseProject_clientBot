6eedb8b903f368c0f0ee7e81aec7c8252771c956
ilya3052 <ipopadin5@gmail.com>
2025-06-08 21:21:06 +0300

    ref: отключение вывода

--- first_launch.bat
+++ first_launch.bat
@@ -1,4 +1,4 @@
-@echo on
+@echo off
 
 python -m venv .venv
 call .venv\Scripts\activate.bat
@@ -6,7 +6,7 @@ pip install -r requirements.txt
 
 mkdir logs
 
-echo @echo on > run.bat
+echo @echo off > run.bat
 echo call .venv\Scripts\activate.bat >> run.bat
 echo python main.py >> run.bat
 
