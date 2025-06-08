dff5f7a950ff81353f062b739cdb9b9b9df5f0fc
ilya3052 <ipopadin5@gmail.com>
2025-06-08 20:33:42 +0300

    add: создание пустого .env

--- first_launch.bat
+++ first_launch.bat
@@ -10,4 +10,8 @@ echo @echo on > run.bat
 echo call .venv\Scripts\activate.bat >> run.bat
 echo python main.py >> run.bat
 
+if not exist ".env" (
+    copy .env.example .env 2>nul || echo Заполните .env на основе .env.example!
+)
+
 del "%~f0"
