89aafbe50ca94a3669f278fda3aa17267bfa2472
ilya3052 <ipopadin5@gmail.com>
2025-06-08 20:30:22 +0300

    add: создание папки логов

--- first_launch.bat
+++ first_launch.bat
@@ -4,6 +4,8 @@ python -m venv .venv
 call .venv\Scripts\activate.bat
 pip install -r requirements.txt
 
+mkdir logs
+
 echo @echo on > run.bat
 echo call .venv\Scripts\activate.bat >> run.bat
 echo python main.py >> run.bat
